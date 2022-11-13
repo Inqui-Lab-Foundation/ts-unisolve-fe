d6d35fc7c4e1834db12e66d65f3665541666fdfa
Neeraj Shukla <neeraj@neoscript.in>
2022-11-14 03:52:57 +0530

    only once greeting should be happne

--- src/Student/PreSurvey/PreSurvey.js
+++ src/Student/PreSurvey/PreSurvey.js
@@ -69,7 +69,7 @@ const PreSurvey = () => {
         (state) => state?.studentRegistration?.studentLanguage
     );
     const [show, setShow] = useState(false);
-    const [greetChildrensDay, setGreetChildrensDay] = useState(true);
+    const [greetChildrensDay, setGreetChildrensDay] = useState(false);
 
     const formik = useFormik({
         initialValues: {},
@@ -158,6 +158,13 @@ const PreSurvey = () => {
     const handleClose = () => {
         setGreetChildrensDay(false);
     };
+
+    useEffect(() => {
+        if (!localStorage.getItem('greetingChildren')) {
+            localStorage.setItem('greetingChildren', true);
+            setGreetChildrensDay(true);
+        }
+    }, []);
 
     return (
         <Layout>
