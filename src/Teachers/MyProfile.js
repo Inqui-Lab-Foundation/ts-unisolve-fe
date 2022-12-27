7c7192290bad1e14ba445bb9893370ad1b415dd7
Ramant-Burri <ramant@inqui-lab.org>
2022-12-27 17:46:20 +0530

    removing principal name,mobile,email

--- src/Teachers/MyProfile.js
+++ src/Teachers/MyProfile.js
@@ -268,7 +268,7 @@ const MyProfile = () => {
                                                                 </Col>
                                                             </Row>
 
-                                                            <Row className="pt-3 pb-3">
+                                                            {/* <Row className="pt-3 pb-3">
                                                                 <Col
                                                                     md={5}
                                                                     className="my-auto profile-detail"
@@ -293,8 +293,8 @@ const MyProfile = () => {
                                                                             : '-'}
                                                                     </b>
                                                                 </Col>
-                                                            </Row>
-                                                            <Row className="pt-3 pb-3">
+                                                            </Row> */}
+                                                            {/* <Row className="pt-3 pb-3">
                                                                 <Col
                                                                     md={5}
                                                                     className="my-auto profile-detail"
@@ -319,9 +319,9 @@ const MyProfile = () => {
                                                                             : '-'}
                                                                     </b>
                                                                 </Col>
-                                                            </Row>
+                                                            </Row> */}
 
-                                                            <Row className="pt-3 pb-3">
+                                                            {/* <Row className="pt-3 pb-3">
                                                                 <Col
                                                                     md={5}
                                                                     className="my-auto profile-detail"
@@ -346,7 +346,7 @@ const MyProfile = () => {
                                                                             : '-'}
                                                                     </b>
                                                                 </Col>
-                                                            </Row>
+                                                            </Row> */}
 
                                                             <Row className="pt-3 pb-3">
                                                                 <Col
